586e86b995040967eb8dfac13781cc7e9ceb573f
GiaKhangLuu <waoknight@gmail.com>
2024-03-13 11:17:56 +0700

    complete all config to train yolof_resnet_50_1x

--- khang_net/configs/yolof/yolof_resnet_50_1x.py
+++ khang_net/configs/yolof/yolof_resnet_50_1x.py
@@ -1,12 +1,13 @@
-from detectron2.model_zoo.configs.common.optim import SGD as optimizer
-from detectron2.model_zoo.configs.common.coco_schedule import lr_multiplier_1x as lr_multiplier
-from detectron2.model_zoo.configs.common.data.coco import dataloader
 from detectron2.model_zoo.configs.common.train import train
 
 from khang_net.configs.yolof.base_yolof import model
+from khang_net.configs.yolof.optim import YOLOF_SGD as optimizer
+from khang_net.configs.yolof.coco_schedule import lr_multiplier_1x as lr_multiplier
+from khang_net.configs.yolof.coco_dataloader import dataloader
 
 dataloader.train.mapper.use_instance_mask = False
-#model.backbone.bottom_up.freeze_at = 2
-optimizer.lr = 0.01
+
+model.backbone.freeze_at = 2
 
 train['init_checkpoint'] = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
+train['max_iter'] = 22500

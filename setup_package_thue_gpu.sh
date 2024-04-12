da36bdb4f257e69be4739cfc5955ba072fa655cb
GiaKhangLuu <waoknight@gmail.com>
2024-04-12 10:56:25 +0700

    change something

--- setup_package_thue_gpu.sh
+++ setup_package_thue_gpu.sh
@@ -1,9 +1,5 @@
 pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
 
-git clone https://github.com/GiaKhangLuu/hope_to_public_net.git
-
-cd hope_to_public_net
-
 python3 -m pip install -e detectron2
 
 scp -r -P 11277 giakhang@0.tcp.ap.ngrok.io:/media/giakhang/146096A860968FDA/autopilot_giakhang/dataset/cityscapes2/cityscapes .

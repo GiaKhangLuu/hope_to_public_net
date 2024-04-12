26febe08bcf275c0eacfd84e4da3bac113a0d556
GiaKhangLuu <waoknight@gmail.com>
2024-04-12 12:17:24 +0700

    install some package that do not exist on thuegpu.vn

--- setup_package_thue_gpu.sh
+++ setup_package_thue_gpu.sh
@@ -1,6 +1,10 @@
+apt install unzip
+
 pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
 
 python3 -m pip install -e detectron2
+
+python3 -m pip install opencv_python
 
 python3 -m pip install gdown
 

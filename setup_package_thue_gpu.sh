aff46cb63e9133b61aa2aa272d2e5c3adbc70daa
GiaKhangLuu <waoknight@gmail.com>
2024-04-15 23:18:35 +0700

    change sth in setup_package.sh file

--- setup_package_thue_gpu.sh
+++ setup_package_thue_gpu.sh
@@ -2,6 +2,10 @@ apt install unzip
 
 pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
 
+git clone https://github.com/GiaKhangLuu/hope_to_public_net.git
+
+cd hope_to_public_net
+
 python3 -m pip install -e detectron2
 
 python3 -m pip install opencv_python
@@ -14,17 +18,10 @@ gdown 1Mqz_cIt8KCplKA-cYkCXoCEjoYk-MOPw
 unzip huflit_net_20k_iters.zip
 unzip cityscapes2.zip
 
-ln cityscapes2/cityscapes ./cityscapes
-
-#scp -r -P 11277 giakhang@0.tcp.ap.ngrok.io:/media/giakhang/146096A860968FDA/autopilot_giakhang/dataset/cityscapes2/cityscapes .
-
-#scp -r -P 11277 giakhang@0.tcp.ap.ngrok.io:/media/giakhang/146096A860968FDA/autopilot_giakhang/weights/huflit_net_20k_iters .
-
 mkdir output
 
-ln ./huflit_net_20k_iters/last_checkpoint ./output
-ln ./huflit_net_20k_iters/model_final.pth ./output
+# Using `cp` instead of `ln`. The os may not detect symbolic files
+cp ./huflit_net_20k_iters/last_checkpoint ./output
+cp ./huflit_net_20k_iters/model_final.pth ./output
 
 python3 train_huflitnet.py
-
-scp -r -P 11277 ./output giakhang@0.tcp.ap.ngrok.io:/home/giakhang/Downloads

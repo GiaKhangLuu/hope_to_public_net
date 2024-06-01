8f7a73de8f73e683b6e009c2adafd8a49fdbee77
GiaKhangLuu <waoknight@gmail.com>
2024-06-01 22:11:22 +0800

    add more conv in mask head

--- khang_net/modeling/mask_head.py
+++ khang_net/modeling/mask_head.py
@@ -264,26 +264,9 @@ class MaskRCNNConvUpsampleHead(BaseMaskRCNNHead, nn.Sequential):
         if self.predictor.bias is not None:
             nn.init.constant_(self.predictor.bias, 0)
 
-    @classmethod
-    def from_config(cls, cfg, input_shape):
-        ret = super().from_config(cfg, input_shape)
-        conv_dim = cfg.MODEL.ROI_MASK_HEAD.CONV_DIM
-        num_conv = cfg.MODEL.ROI_MASK_HEAD.NUM_CONV
-        ret.update(
-            conv_dims=[conv_dim] * (num_conv + 1),  # +1 for ConvTranspose
-            conv_norm=cfg.MODEL.ROI_MASK_HEAD.NORM,
-            input_shape=input_shape,
-        )
-        if cfg.MODEL.ROI_MASK_HEAD.CLS_AGNOSTIC_MASK:
-            ret["num_classes"] = 1
-        else:
-            ret["num_classes"] = cfg.MODEL.ROI_HEADS.NUM_CLASSES
-        return ret
-
     def layers(self, x):
         for i, layer in enumerate(self.conv_norm_relus):
-            if (i > 0 and 
-                len(self.conv_norm_relus) // i == 2 and 
+            if (i == (len(self.conv_norm_relus) - 1) and 
                 self.spatial_attention):
                 x = self.spatial_attention(x)
             x = layer(x)
